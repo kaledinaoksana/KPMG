dcf867330299f180f9c973a0ff52bd8666d11699
‘kaledinaoa’ <kaledinaoa@gmail.com>
2023-08-13 20:01:11 +0400

    add settings

---
+++ settings.py
@@ -0,0 +1,5 @@
+files_folder = '/data/new_files/'
+file_dem = files_folder + 'demographic.csv'
+file_address = files_folder + 'address.csv'
+file_trans = files_folder + 'transactions.csv'
+source = '/data/source/data.xlsx'
